ac53a0d19d250de3842bc97d45108b3c716272f1
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-03-20 14:18:24 +0100

    Init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# GRUPOP DE BAILE
---
+++ src/test/java/com/dancingcoders/grupobaile/GrupobaileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dancingcoders.grupobaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupobaileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
